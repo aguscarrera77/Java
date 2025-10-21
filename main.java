f4d733c4c376b1babea38c6d98d74091fa4b70d4
aguscarrera77 <aguscarrera77@gmail.com>
2025-10-21 11:43:50 -0300

    20/10

---
+++ main.java
@@ -0,0 +1,25 @@
+//primer archivo de java.
+
+
+public class main {
+    public static void main(String[] args) {
+        //que el bloque de codigo se ejecutara despues de la entrada al programa.
+        System.out.println("Hola a todos");
+    }
+}
+
+//variables. es un estructura de nuestro codigo que guarda valores.
+//int (declarar valores enteros).Ej: int edad=48.
+//double(valores con decimales). Ej: double pi=3.14
+//boolean(valores booleanos)Ej: boolean clase= true
+//string (valores son textos) Ej: string nombre="Agustin"
+
+public class Innermain {
+    public static void main(String[] args) {
+        String nombre="Agustin";
+        int edad=48;
+        boolean estudiante=true;
+    }
+
+    
+}

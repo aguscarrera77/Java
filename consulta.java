c8bb26616a8d79c7d28289274ab2915f3bf9a99a
aguscarrera77 <aguscarrera77@gmail.com>
2025-10-22 20:27:12 -0300

    22/10

---
+++ consulta.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class consulta {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Ingrese un numero");
+        int number1=sc.nextInt();
+        System.out.println("Ingrese un segundo numero");
+        int number2=sc.nextInt();
+        int suma=number1 + number2;
+        System.out.print("La suma es: "+ suma);
+        sc.close();
+    }
+}
--- main.java
+++ main.java
@@ -3,23 +3,31 @@
 
 public class main {
     public static void main(String[] args) {
-        //que el bloque de codigo se ejecutara despues de la entrada al programa.
         System.out.println("Hola a todos");
-    }
+    String nombre="Roberto";
+    int edad=108;
+    boolean instructor=true;
+    double estatura=1.85;
+    System.out.println("Mi nombre es "+ nombre);
+    System.out.println("Tengo "+ edad +" anos");
+    System.out.println(instructor);
+    System.out.println("Mido "+ estatura+"metros");
+    //Calcular promedio de 3 notas.
+    double nota1=9.8;
+    double nota2=8.5;
+    double nota3=4.6;
+    double promedio=(nota1+nota2+nota3)/3;
+    System.out.println("Su nota final es :"+ promedio);
+    //Preguntar datos al usuario.
+    
+
+    }    
 }
 
-//variables. es un estructura de nuestro codigo que guarda valores.
+//variables. es un estructura de nuestro codigo que guarda valores o informacion.
 //int (declarar valores enteros).Ej: int edad=48.
 //double(valores con decimales). Ej: double pi=3.14
 //boolean(valores booleanos)Ej: boolean clase= true
 //string (valores son textos) Ej: string nombre="Agustin"
 
-public class Innermain {
-    public static void main(String[] args) {
-        String nombre="Agustin";
-        int edad=48;
-        boolean estudiante=true;
-    }
 
-    
-}
